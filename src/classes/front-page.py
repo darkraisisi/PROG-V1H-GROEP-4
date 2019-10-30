c5f7597debf18b43b06c3f52cef437970397abfd
dtmartha <dmartha@roc-dev.com>
2019-10-30 17:20:26 +0100

    Docstrings toegevoegd.

--- src/classes/front-page.py
+++ src/classes/front-page.py
@@ -4,10 +4,17 @@ import os
 
 root = Tk()
 
+'''Default grootte van het scherm'''
+
 w = '400'
 h = '100'
 root.geometry('{}x{}'.format(w, h))
+
+'''Achtergrond kleur'''
+
 root.configure(background='#FCC63F')
+
+'''Bovenste label 'welkom bij NS' '''
 
 label = Label(root, text="Welkom bij NS", background='#FCC63F',
             foreground='#212B5C',
@@ -18,9 +25,13 @@ label = Label(root, text="Welkom bij NS", background='#FCC63F',
 label.grid(column=0, row=0)
 root.columnconfigure(0, weight=1)
 
+'''Logo van NS'''
+
 img = ImageTk.PhotoImage(Image.open("../images/ns-logo.jpg"))
 label2 = Label(root, image=img)
 label2.grid(column=0, row=1)
+
+'''Reisinformatie button'''
 
 button3 = Button(master=root, text='Toon reisinformatie', bg='#212B5C',font=('Helvetica', 23, 'bold'))
 button3.grid(row=2, column=0, pady=32)
